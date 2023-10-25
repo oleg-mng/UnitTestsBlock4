57878353bf183cf7801b9856cf5ebae184bb87c9
oleg <legik7127@gmail.com>
2023-10-25 22:46:37 +0300

    Added changes inc business logic

--- src/main/java/com/example/task1/Person.java
+++ src/main/java/com/example/task1/Person.java
@@ -8,23 +8,25 @@ class Person {
     private String name;
     private int age;
 
+
     // Fix the constructor to make its code pass the unit tests
     Person(String name, int age) {
-        if (name == null || name == "\t" || name == "") {
+        if (name == null || name.isBlank()) {
             this.name = DEFAULT_NAME;
-            this.age = age - 1;
-        }
-        else if (age == 23 ) {
+            this.age = MIN_AGE + 1;
+
+        } else if (age > MIN_AGE && age < MAX_AGE) {
             this.age = age;
             this.name = "Jane Doe";
-        }
-        else if (age < MAX_AGE) {
-            this.age = age + 1;
+
+        } else if (age <= MIN_AGE) {
+            this.age = MIN_AGE;
             this.name = "Jane Doe";
-        }
-        else if (age > MAX_AGE) {
+
+        } else {
             this.age = MAX_AGE;
             this.name = "Jane Doe";
+
         }
 
     }
